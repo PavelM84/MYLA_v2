51e016490cab73286e61d78c448a75f6e8f10701
PavelM84 <pavel.a.melnikov@gmail.com>
2024-12-28 00:11:44 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-API_TOKEN = 'BOT_TOKEN'
+BOT_TOKEN = '7959132994:AAFo3r9-EhvUUkxpi7kDfU7BZaBMHTyTqdU'
 
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 # Хэндлер для команды /start
