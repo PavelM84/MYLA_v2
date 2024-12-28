8fce86fa2bf5d06d0c32eec71e254a3630bfdd43
PavelM84 <pavel.a.melnikov@gmail.com>
2024-12-29 00:00:04 +0400

    Update url_storage.py

--- url_storage.py
+++ url_storage.py
@@ -1,7 +1,15 @@
 import json
 import os
 
+#
+import subprocess
+#
+
 URL_STORAGE_FILE = "url_storage_json"
+
+#
+COOKIES_FILE = "cookies.txt"
+#
 
 def load_url_storage():
     if os.path.exists(URL_STORAGE_FILE):
@@ -12,5 +20,21 @@ def load_url_storage():
 def save_url_storage(data):
     with open(URL_STORAGE_FILE, "w") as file:
         json.dump(data, file)
+
+
+#
+def download_video(url):
+    # Команда для yt-dlp с использованием cookies
+    command = [
+        "yt-dlp",
+        "--cookies", COOKIES_FILE,  # Передача файла cookies
+        url
+    ]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при загрузке видео: {e}")
+#
+
 
 url_storage = load_url_storage()
