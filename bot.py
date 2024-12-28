d21bfa8b5a7358d4fd0bc465607ca066f06a8e3e
PavelM84 <pavel.a.melnikov@gmail.com>
2024-12-28 23:44:30 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,43 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
+#1
+#import telebot
+#/1
+
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения из .env
-load_dotenv()
+from handlers import callaback, commands
 
-# Токен бота, извлекаемый из .env
-API_TOKEN = os.getenv('BOT_TOKEN')
+#2
 
-# Создаем экземпляры бота и диспетчера
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+#@bot.message_handler(commands=['info'])
+#def send_show(message):
+#    bot.reply_to(message, "Вот, что я умею:")
 
-# Хэндлер для команды /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет! Напишите команду 'Login', чтобы открыть YouTube.")
+#2/
 
-# Хэндлер для команды 'Login'
-@dp.message_handler(lambda message: message.text.lower() == 'login')
-async def open_youtube(message: types.Message):
-    youtube_url = "https://www.youtube.com"  # Ссылка на YouTube
-    await message.reply(f"Открываю YouTube для вас: {youtube_url}")
 
-# Функция для запуска бота
+
 async def main():
+    load_dotenv()
+    token = os.getenv('BOT_TOKEN')
+    bot = Bot(token)
+    dp = Dispatcher()
     try:
+        if not os.path.exists("downloads"):
+            os.makedirs("downloads")
+        dp.include_router(commands.router)
+        dp.include_router(callaback.router)
         print('Bot started')
-        # Запуск бота
-        await dp.start_polling()
+        await dp.start_polling(bot)
+        await bot.session.close()
     except Exception as ex:
-        print(f"There is an exception: {ex}")
+        print(f"There is exeption: {ex}")
 
 if __name__ == '__main__':
     try:
-        # Запуск асинхронной функции
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Exit')
+    
