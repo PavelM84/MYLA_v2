f24925e060433a14e24a238720382509151c214a
PavelM84 <pavel.a.melnikov@gmail.com>
2024-12-28 23:45:07 +0400

    Update function.py

--- handlers/function.py
+++ handlers/function.py
@@ -5,19 +5,29 @@ import time
 from aiogram.types import FSInputFile
 
 
+
 def generate_url_id(url: str):
     return hashlib.md5(url.encode()).hexdigest()
 
 
 async def download_and_send_media(bot, chat_id, url, media_type):
+     
     ydl_opts = {
         'format': 'best[height<=480]' if media_type == 'video' else 'bestaudio/best',
-        'outtmpl': f"downloads/%(title)s.{'mp4' if media_type == 'video' else 'm4a'}",
-        'cookies_from_browser': 'Chrome',  # Используем куки из браузера Chrome
-        # Если вам нужно использовать конкретные куки, укажите файл с куками с помощью '--cookies'
-        # 'cookies': '/path/to/cookies.txt',  # Пример использования куки файла
-    }
+       'outtmpl': f"downloads/%(title)s.{'mp4' if media_type == 'video' else 'm4a'}",
+    } 
 
+#    ydl_opts = {
+#        'format': 'best[exp=mp4]' if media_type == 'video' else 'bestaudio[exp=m4a]/best',
+#        'outtmpl': f'downloads/%(title)s.{'mp4' if media_type == 'video' else 'm4a'}',
+
+# попытка номер 2  --          потом разберксь
+#ydl_opts = {
+#   'format': 'best',  # Автоматический выбор лучшего доступного формата
+#        'format': 'best[exp=mp4]' if media_type == 'video' else 'bestaudio[exp=m4a]/best',
+#       'outtmpl': f'downloads/%(title)s.{'mp4' if media_type == 'video' else 'm4a'}',
+#    }
+  
     try:
         start_time = time.time()
 
